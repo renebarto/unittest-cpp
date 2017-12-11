2613c8c7cac5c96a59ef2afb5806a43982258d9e
Rene Barto <rene.barto66@gmail.com>
2017-12-11 11:58:45 +0100

    Added copyright and license information.

--- export/unittest-c++/UnitTestC++.h
+++ export/unittest-c++/UnitTestC++.h
@@ -1,3 +1,38 @@
+// Unit testing framework for C++
+//
+// Based on google test and UnitTest++
+//
+// UnitTest-c++ is based on google test (for printing values and test definitions), and UnitTest++.
+//
+// MIT License
+//
+// Copyright (c) 2017 Rene Barto
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+// the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Copyright
+//
+// For Google test:
+//
+// Copyright 2008, Google Inc.
+// All rights reserved.
+//
+// For UnitTest++:
+//
+// Copyright (c) 2006 Noel Llopis and Charles Nicholson
+
 #pragma once
 
 #include <unittest-c++/AssertionFailedException.h>
@@ -17,18 +52,18 @@ namespace UnitTestCpp
 UNIT_TEST_CPP_EXPORT int RunAllTests();
 UNIT_TEST_CPP_EXPORT int RunAllTests(ITestReporter & reporter);
 
-template <class Predicate>
+template<class Predicate>
 int RunSelectedTests(const Predicate & predicate)
 {
-	ConsoleTestReporter reporter;
-	return RunSelectedTests(reporter, predicate);
+    ConsoleTestReporter reporter;
+    return RunSelectedTests(reporter, predicate);
 }
 
-template <class Predicate>
+template<class Predicate>
 int RunSelectedTests(ITestReporter & reporter, const Predicate & predicate)
 {
-	TestRunner runner(reporter);
-	return runner.RunTestsIf(Test::GetTestRegistry(), predicate, 0);
+    TestRunner runner(reporter);
+    return runner.RunTestsIf(Test::GetTestRegistry(), predicate, 0);
 }
 
 } // namespace UnitTestCpp
